180dd713c36d39367ce834d8103535f08e018d58
nikhxxt <nikhxtwork@gmail.com>
2025-10-21 17:20:29 +0530

    Replace Firebase config with environment variables

--- parent_dashboard/src/firebase.js
+++ parent_dashboard/src/firebase.js
@@ -2,15 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCoDFCwIdLOg9PhDH7ggTQBjHtZK1xRbgk",
-  authDomain: "parentguard-106c8.firebaseapp.com",
-  projectId: "parentguard-106c8",
-  storageBucket: "parentguard-106c8.firebasestorage.app",
-  messagingSenderId: "50414256935",
-  appId: "1:50414256935:web:6c53309ee650c0aca79f02",
-  measurementId: "G-SGY59CWTYB"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-
