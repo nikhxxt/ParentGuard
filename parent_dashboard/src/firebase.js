36a93fff38decff806227b6fc92fbe3ec68066d1
nikhxxt <nikhxtwork@gmail.com>
2025-10-21 13:00:48 +0530

    Update firebase.js

--- parent_dashboard/src/firebase.js
+++ parent_dashboard/src/firebase.js
@@ -2,12 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "<YOUR_FIREBASE_API_KEY>",
-  authDomain: "<YOUR_FIREBASE_AUTH_DOMAIN>",
-  projectId: "<YOUR_FIREBASE_PROJECT_ID>"
+  apiKey: "AIzaSyCoDFCwIdLOg9PhDH7ggTQBjHtZK1xRbgk",
+  authDomain: "parentguard-106c8.firebaseapp.com",
+  projectId: "parentguard-106c8",
+  storageBucket: "parentguard-106c8.firebasestorage.app",
+  messagingSenderId: "50414256935",
+  appId: "1:50414256935:web:6c53309ee650c0aca79f02",
+  measurementId: "G-SGY59CWTYB"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
+export const db = getFirestore(app);
